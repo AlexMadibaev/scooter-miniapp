a391a6e1793c8b891e258adf050ecb7c507872c6
Alexandr <a.madibaev@gmail.com>
2025-07-20 06:53:13 +0500

    Fix React version compatibility for Netlify deployment

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Button, Box, Typography } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
 
 interface MainMenuProps {
   onNavigate: (path: string) => void;
